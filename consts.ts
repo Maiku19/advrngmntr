68fab4e36bf3a41f152b129b438122233b3fb98c
Maiku <91633676+Maiku19@users.noreply.github.com>
2024-05-02 10:54:31 +0200

    fixed typo in const recordingsDir

--- consts.ts
+++ consts.ts
@@ -2,4 +2,4 @@ export const logDir: string = "./logs";
 export const oldLogDir: string = `${logDir}/old`;
 export const latestLog: string = `${logDir}/latest.log`;
 
-export const recordingsDir: string = `$./recordings`;
+export const recordingsDir: string = `./recordings`;
